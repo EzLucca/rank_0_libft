42df9ce476e8cc7552d589be1a615548b7f69a16
Eduardo Lucca <edlucca@student.hive.fi>
2025-04-22 19:38:19 +0300

    Added tests for bonus

---
+++ tests/ft_striteri-main.c
@@ -0,0 +1,33 @@
+
+#include <string.h>
+#include <stdio.h>
+#include "../libft.h"
+
+void	test(int test_nb, char *s, void (*f)(unsigned int, char*))
+{
+    // int i = 0;
+    ft_striteri(s, f);
+
+    printf("Test %d\n", test_nb);
+    printf("String: %s\n", s);
+}
+
+void	my_func(unsigned int i, char *str)
+{
+    if (i % 2 == 0) 
+    {
+        *str -= 32;
+    }
+}
+
+int main(void)
+{
+
+    printf("\n--------ft_striteri--------\n");
+    test(1, strdup("this"), my_func);
+    test(2, strdup("the"), my_func);
+    test(3, strdup(""), my_func);
+    test(4, strdup("between"), my_func);
+    test(5, strdup("     "), my_func); 
+    return(0);	
+}
